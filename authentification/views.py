47165fcf01b6e26a0fc948b442e988c0bd6c22be
bendah123 <121314806+bendah123@users.noreply.github.com>
2023-12-12 15:10:42 +0200

    Update views.py

--- authentification/views.py
+++ authentification/views.py
@@ -65,9 +65,9 @@ def signup(request):
         return redirect('signup')'''
         date_joined = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         #FLAW 4 A02:2021-Cryptographic Failures
-        password1 =pass1
+        #password1 =pass1
         #Solution Flaw 4 
-        #password1 = make_password(pass1)
+        password1 = make_password(pass1)
         #FLAW 2 A03:2021:Injection
         query = f"INSERT INTO auth_user (username, first_name, last_name, email, password, is_superuser, is_staff, is_active, date_joined) VALUES ('{username}', '{fname}', '{lname}', '{email}', '{password1}', 0, 0, 1, '{date_joined}')"
         with connection.cursor() as cursor:
@@ -79,7 +79,8 @@ def signup(request):
         myuser.save()'''
 
         messages.success(request, "Your account has been successfully created")
-        return render(request, 'authentification/welcome.html', {'fname': fname})
+        return redirect('signin')
+        
     return render(request, "authentification/signup.html")
 
 
@@ -100,11 +101,11 @@ def signin(request):
 
         # Check if the provided password matches the stored password (plaintext)
         #solution FLAW 4
-        #if user.check_password(pass1):
-        if pass1==user.password:
+        if user.check_password(pass1):
+        #if pass1==user.password:
             login(request, user)
             fname = user.first_name
-            return render(request, 'authentification/welcome.html', {'fname': fname})
+            return redirect('welcome', username=user.username)
         else:
             messages.error(request, "Invalid username or password. Please try again.")
             return redirect('home')
@@ -117,3 +118,19 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged Out Successfully")
     return redirect('home')
+
+def welcome(request, username):
+    # Retrieve the user based on the username parameter
+    user = User.objects.get(username=username)
+    #FIX 1 solution:Broken Access Control 
+    '''if not request.user.is_authenticated or request.user != user:
+           messages.error(request, "You are not authorized to access this page.")
+           return redirect('signin')'''
+    fname = user.first_name
+
+    return render(request, 'authentification/welcome.html', {'fname': fname})
+
+
+
+
+ 
