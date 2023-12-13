56ead5a46db6a625134654bf34c77b40d303a9bb
bendah123 <121314806+bendah123@users.noreply.github.com>
2023-12-13 20:22:45 +0200

    Update urls.py

--- authentification/urls.py
+++ authentification/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
+    path('welcome/<username>/', views.welcome, name='welcome'),
 
-
-   
 ]
